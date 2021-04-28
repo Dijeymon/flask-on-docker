f340b966a6c71cb509801dd49baa9970535ad682
Anton Fejer <anton.28@hotmail.com>
2021-04-28 16:48:59 -0300

    The project structure was formed and a dockerfile was added

---
+++ services/web/project/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+
+@app.route("/")
+def hello_world():
+    return jsonify(hello="world")
