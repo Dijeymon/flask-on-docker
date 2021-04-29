6a177e6635f7c27de4d2d984ef57cb91ebd8fc71
Anton Fejer <anton.28@hotmail.com>
2021-04-29 11:35:53 -0300

    Added bcrypt config

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -7,3 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '664e2845018cfae983412ed3'
+    BCRYPT_LOG_ROUNDS = 20
+    BCRYPT_HASH_PREFIX = '2b'
+    BCRYPT_HANDLE_LONG_PASSWORDS = False
